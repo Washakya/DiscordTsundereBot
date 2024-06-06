2a664733a60ebe73d9c2837dc7100b67eea779a0
Washakya <86008644+Washakya@users.noreply.github.com>
2024-06-06 17:49:53 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import math
+import datetime
+
+part = math.floor(datetime.datetime.now().hour / 6)
+
+print("起動:" +  datetime.datetime.now().strftime('%Y年%m月%d日 %H:%M:%S'))
+
+while part ==  math.floor(datetime.datetime.now().hour / 6):
+    pass
+
+print("開始:" +  datetime.datetime.now().strftime('%Y年%m月%d日 %H:%M:%S'))
+
+part = (part + 1) % 6
+
+print(datetime.datetime.now())
+
+while  part == math.floor(datetime.datetime.now().hour / 6) + 1:
+    pass
+
+print("終了:" +  datetime.datetime.now().strftime('%Y年%m月%d日 %H:%M:%S'))
