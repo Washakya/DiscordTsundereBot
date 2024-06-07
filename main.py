db76bc52d6c382a2341fdb655528800fc875b848
Washakya <86008644+Washakya@users.noreply.github.com>
2024-06-07 14:39:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import math
 import datetime
 
-part = math.floor(datetime.datetime.now().hour / 6)
+part = (math.floor(datetime.datetime.now().hour / 4) + 1) % 6
 
 print("起動:" +  datetime.datetime.now().strftime('%Y年%m月%d日 %H:%M:%S'))
 
-while part ==  math.floor(datetime.datetime.now().hour / 6):
+while part !=  math.floor(datetime.datetime.now().hour / 4):
     pass
-
+print(part)
 print("開始:" +  datetime.datetime.now().strftime('%Y年%m月%d日 %H:%M:%S'))
 
-part = (part + 1) % 6
-
-while  part == math.floor(datetime.datetime.now().hour / 6):
+while  part == math.floor(datetime.datetime.now().hour / 4):
     pass
 
 print("終了:" +  datetime.datetime.now().strftime('%Y年%m月%d日 %H:%M:%S'))
